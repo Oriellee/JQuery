01b7a83d08c9635d4e7c0f7ce1b30e9661836fd7
Oriel <1102386151@qq.com>
2017-10-20 19:01:58 +0800

    完成。

--- XER/scripts/input.js
+++ XER/scripts/input.js
@@ -1,22 +1,35 @@
-$(function(){
-	$("#search input").hover(function() {
-		$(this).stop(true,true)
-		.animate({width:"200"}, 200);
-	},function(){
-		$(this).stop(true,true)
-		.animate({width:"150"}, 200);
-	});
-	$("#search input").focus(function(event) {
-		if($(this).val() == this.defaultValue){
-			$(this).val("");
-		}
-	}).blur(function(event) {
-		if ($(this).val() == "") {
-			$(this).val(this.defaultValue);
-		}
-	}).keyup(function(e) {
-		if(e.which == 13){
-			alert("回车提交表单！");
-		}
-	});
+$(function() {
+    $("#search input").hover(function() {
+        $(this).stop(true, true)
+            .animate({ width: "200" }, 200);
+    }, function() {
+        $(this).stop(true, true)
+            .animate({ width: "150" }, 200);
+    });
+    $("#search input,#secondInput input").focus(function(event) {
+        if ($(this).val() == this.defaultValue) {
+            $(this).val("");
+        }
+    }).blur(function(event) {
+        if ($(this).val() == "") {
+            $(this).val(this.defaultValue);
+        }
+    }).keyup(function(e) {
+        if (e.which == 13) {
+            alert("回车提交表单！");
+        }
+    });
+    $("#search img").click(function(event) {
+        if ($("#search input").css('display') == "none") {
+            $("#secondInput").show();
+            $("#secondInput input").focus();
+        } else {}
+    });
+    $("#secondInput").focusout(function(event) {
+        $(this).hide();
+    });
+    $(window).resize(function() {
+        $("#secondInput").hide();
+
+    });
 })
--- XER/scripts/nav.js
+++ XER/scripts/nav.js
@@ -1,7 +1,34 @@
-$(function(){
-	$("#menu li").hover(function() {
-		$(this).find('.nav').show();
-	}, function() {
-		$(this).find('.nav').hide();
-	});
+$(function() {
+    $("#menu li").hover(function() {
+        $(this).find('.nav').show();
+    }, function() {
+        $(this).find('.nav').hide();
+    });
+
+    function check(width) {
+        if (width < 768) {
+            $("#smallMenu").show();
+            $("#menu").appendTo($("#newMenu"));
+            $("#header").css('height', '6rem');
+        } else {
+            $("#header").css('height', '9rem');
+            $("#menu").appendTo($("#header"));
+            $("#smallMenu").hide();
+            $("#newMenu").hide();
+        }
+    }
+    check($(window).width());
+    $(window).resize(function() {
+        var width = $(this).width();
+        check(width);
+    });
+    $("#smallMenu").click(function(event) {
+        if ($("#newMenu").css("display") == "none") {
+            $("#newMenu").show();
+
+        } else {
+            $("#newMenu").hide();
+        }
+    });
+
 })
